bc80e8d9f6e07c153478848b5b8ccabbaf0a0315
dhan basnet <dhanbasnet90@gmail.com>
2020-11-05 16:57:03 -0500

    Initial Commit

---
+++ assets/main.js
@@ -0,0 +1,57 @@
+let dieRollsInput = document.querySelector("#roll-number");
+let rollButton = document.querySelector("#roll-dice");
+let showAllRolls = document.querySelector("#Show-All-Rolls");
+let rollResults = document.querySelector("#all-rolls");
+let additionTotal = document.querySelector("#total-number");
+let allRolls = [];
+let rolls = [];
+
+rollButton.addEventListener("click", function() { 
+    console.log(dieRollsInput.value);
+    console.log(rollButton<0)
+    let maxRolls = Number(dieRollsInput.value);
+    let count = 0;
+
+while(count < maxRolls) {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+allRolls.push(randomNumber);
+    count++;
+}
+console.log(allRolls)
+})
+    
+showAllRolls.addEventListener("click", function() {
+    rollResults.innerHTML = ""
+    let count = 0;
+    while(count < allRolls.length) {
+        console.log(allRolls[count]);
+        rollResults.innerHTML += "<li>" + allRolls[count] + "</li>"
+        count++;
+    }
+})
+
+
+
+let roll = [];
+let count = 0;
+
+while(count <= 16) {
+    rolls.push(Math.floor(Math.random() * 6) +1);
+    count++;
+}
+
+let total = 0;
+let i = 0
+
+while( i < rolls.length) {
+    console.log(rolls[i]);
+    total += roll[i];
+    i++;
+
+}
+additionTotal.innerHTML = total;
+ 
+console.log(rolls);
+console.log(16);
+
+
